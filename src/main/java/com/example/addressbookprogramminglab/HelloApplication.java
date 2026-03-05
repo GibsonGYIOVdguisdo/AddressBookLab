3e7a064b88716d981a8b967224fb4e1a5aa5d88d
Gibson <114831858+GibsonGYIOVdguisdo@users.noreply.github.com>
2026-03-05 12:12:43 +0000

    Created ui for Entry tab

--- src/main/java/com/example/addressbookprogramminglab/HelloApplication.java
+++ src/main/java/com/example/addressbookprogramminglab/HelloApplication.java
@@ -2,13 +2,11 @@ package com.example.addressbookprogramminglab;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
-import javafx.scene.control.Tab;
-import javafx.scene.layout.BorderPane;
+import javafx.scene.control.*;
+import javafx.scene.layout.*;
 import javafx.stage.Stage;
-import javafx.scene.control.TabPane;
-import javafx.scene.layout.StackPane;
-import javafx.scene.layout.TilePane;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Circle;
 import javafx.stage.Stage;
@@ -16,6 +14,13 @@ import javafx.stage.Stage;
 import java.io.IOException;
 
 public class HelloApplication extends Application {
+    enum Country {
+        UK,
+        US,
+        DE,
+        CN
+    }
+
     @Override
     public void start(Stage stage) throws IOException {
         BorderPane root = new BorderPane();
@@ -23,23 +28,76 @@ public class HelloApplication extends Application {
         stage.setTitle("Address Book");
 
         TabPane tabPane = new TabPane();
-        Tab t1 = new Tab("Entry");
-        Tab t2 = new Tab("List");
-        Tab t3 = new Tab("Search");
-        
-        tabPane.getTabs().add(t1);
-        tabPane.getTabs().add(t2);
-        tabPane.getTabs().add(t3);
 
-        t1.setClosable(false);
-        t2.setClosable(false);
-        t3.setClosable(false);
+        Tab entryTab = new Tab("Entry");
+        Tab listTab = new Tab("List");
+        Tab searchTab = new Tab("Search");
+
+        entryTab.setClosable(false);
+        listTab.setClosable(false);
+        searchTab.setClosable(false);
+
+        tabPane.getTabs().add(entryTab);
+        tabPane.getTabs().add(listTab);
+        tabPane.getTabs().add(searchTab);
 
         root.setLeft(tabPane);
+
+        TilePane entryPane = getEntryPane();
+
+        entryTab.setContent(entryPane);
 
 
         stage.setScene(scene);
         stage.show();
+    }
+
+    TilePane getEntryPane(){
+        TilePane entryPane = new TilePane();
+        entryPane.setPrefColumns(1);
+
+        TilePane inputPane = new TilePane();
+        inputPane.setPrefColumns(2);
+
+        Label nameLabel = new Label("Name: ");
+        Label numberLabel = new Label("Number: ");
+        Label addressLabel = new Label("Address: ");
+        Label countryLabel = new Label("Country: ");
+
+        TextField nameTextField = new TextField();
+        TextField numberTextField = new TextField();
+        TextField addressTextField = new TextField();
+
+        ComboBox<Country> countryComboBox = new ComboBox<>();
+        countryComboBox.getItems().addAll(Country.values());
+
+        inputPane.getChildren().add(nameLabel);
+        inputPane.getChildren().add(nameTextField);
+        inputPane.getChildren().add(numberLabel);
+        inputPane.getChildren().add(numberTextField);
+        inputPane.getChildren().add(addressLabel);
+        inputPane.getChildren().add(addressTextField);
+        inputPane.getChildren().add(countryLabel);
+        inputPane.getChildren().add(countryComboBox);
+
+        HBox buttonHBox = new HBox();
+
+        Button clearButton = new Button("Clear");
+        Button editButton = new Button("Edit");
+        Button addButton = new Button("Add");
+        Button deleteButton = new Button("Delete");
+
+        buttonHBox.getChildren().add(clearButton);
+        buttonHBox.getChildren().add(editButton);
+        buttonHBox.getChildren().add(addButton);
+        buttonHBox.getChildren().add(deleteButton);
+
+        entryPane.getChildren().add(inputPane);
+
+        buttonHBox.setAlignment(Pos.BASELINE_CENTER);
+        entryPane.getChildren().add(buttonHBox);
+
+        return entryPane;
     }
 
     public static void main(String[] args) {
