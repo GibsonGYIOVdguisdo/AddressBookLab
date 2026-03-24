7b926d1883fdc4be4b503edb56980e6b41303f97
GibsonGYIOVdguisdo <Gibson570@hotmail.com>
2026-03-24 20:56:54 +0000

    Implemented Serializable interface and methods to utilise it

--- src/main/java/com/example/addressbookprogramminglab/ContactsModel.java
+++ src/main/java/com/example/addressbookprogramminglab/ContactsModel.java
@@ -1,19 +1,42 @@
 package com.example.addressbookprogramminglab;
 
 import javafx.beans.InvalidationListener;
+import javafx.beans.property.SimpleStringProperty;
 import javafx.collections.FXCollections;
 import javafx.collections.ListChangeListener;
 import javafx.collections.ObservableList;
 
+import java.io.*;
+import java.lang.reflect.Array;
 import java.util.Collection;
 import java.util.Iterator;
 import java.util.List;
 import java.util.ListIterator;
 
-public class ContactsModel {
-    ObservableList<Contact> data = FXCollections.observableArrayList();
+public class ContactsModel implements Serializable {
+    transient ObservableList<Contact> data = FXCollections.observableArrayList();
 
+    ContactsModel(){}
 
+    ContactsModel(Contact[] contacts){
+        data.addAll(contacts);
+    }
+
+    public void saveToFile() throws IOException {
+        FileOutputStream fileOut = new FileOutputStream("file.ser");
+        ObjectOutputStream out = new ObjectOutputStream(fileOut);
+        out.writeObject(this);
+        out.close();
+        fileOut.close();
+    }
+
+    public void loadFromFile() throws IOException, ClassNotFoundException {
+        FileInputStream fileIn = new FileInputStream("file.ser");
+        ObjectInputStream in = new ObjectInputStream(fileIn);
+        this.data.setAll(((ContactsModel) in.readObject()).getContacts().toArray(new Contact[0]));
+        in.close();
+        fileIn.close();
+    }
 
     public void addContact(Contact contact) {
         data.add(contact);
@@ -35,5 +58,17 @@ public class ContactsModel {
             }
         }
         return null;
+    }
+
+    private void writeObject(ObjectOutputStream out) throws IOException {
+        out.defaultWriteObject();
+        Contact[] contacts = this.data.toArray(new Contact[0]);
+        out.writeObject(contacts);
+    }
+
+    private void readObject(ObjectInputStream in) throws IOException, ClassNotFoundException {
+        in.defaultReadObject();
+        Contact[] contacts = (Contact[]) in.readObject();
+        this.data = new ContactsModel(contacts).getContacts();
     }
 }
